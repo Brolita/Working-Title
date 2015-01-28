7083a00cf8dedfcd97a40d034400888a186ef0e1
Anli Ji <anli.azuresand@gmail.com>
2015-01-28 15:23:11 -0500

    Generate Rocks at random position

---
+++ Assets/ARock.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ARock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(Random.Range(-3, 3), transform.position.y, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
