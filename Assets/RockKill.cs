2450a5d936ceb92a2fab10b6c8f1c24756dd208d
brolita <byrnec3@rpi.edu>
2015-01-28 15:13:49 -0500

    Player, Rock prefabs

--- Assets/RockKill.cs
+++ Assets/RockKill.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class RockKill : MonoBehaviour 
 {
-	void OnTriggerEnter(Collider other)
+	void OnTriggerEnter2D(Collider2D other)
 	{
+		Debug.Log ("hello");
 		if(other.tag == "Player")
 		{
-			other.GetComponent<Death>().HandelDeath();
+			//other.GetComponent<Death>().HandelDeath();
 			Destroy(gameObject);
 		}
 	}
