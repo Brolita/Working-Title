d6047db3007b28b8413dc3100f4aef780502939a
Spencer <johnss18@rpi.edu>
2015-01-28 15:11:12 -0500

    heres the SnailMovement doesn't work yet I think

---
+++ Assets/SnailMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnailMovement : MonoBehaviour
+{
+	public bool canMove = true;
+
+	public string forwardKey1;
+	public string forwardKey2;
+	public string backwardKey1;
+	public string backwardKey2;
+
+	public float forwardSpeed;
+	public float backwardSpeed;
+	
+	void Start () 
+	{
+	
+	}
+	void Update () 
+	{
+		if(canMove)
+		{
+			if(Input.GetKeyUp(forwardKey1))
+			{
+				transform.Translate(forwardSpeed,0f,0f);
+			}
+			else if(Input.GetKeyUp(backwardKey1))
+			{
+				transform.Translate(backwardSpeed,0f,0f);
+			}
+		}
+	}
+}
