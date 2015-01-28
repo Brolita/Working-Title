bb6f35a55060c118da8cb00095676ab1972221a9
Spencer <johnss18@rpi.edu>
2015-01-28 15:30:47 -0500

    double key movement

--- Assets/SnailMovement.cs
+++ Assets/SnailMovement.cs
@@ -25,7 +25,15 @@ public class SnailMovement : MonoBehaviour
 			{
 				transform.Translate(forwardSpeed,0f,0f);
 			}
+			else if(Input.GetKeyUp(forwardKey2))
+			{
+				transform.Translate(forwardSpeed,0f,0f);
+			}
 			else if(Input.GetKeyUp(backwardKey1))
+			{
+				transform.Translate(-backwardSpeed,0f,0f);
+			}
+			else if(Input.GetKeyUp(backwardKey2))
 			{
 				transform.Translate(-backwardSpeed,0f,0f);
 			}
