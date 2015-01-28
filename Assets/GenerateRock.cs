8bb841cc56fc37fe483abe34a92d0bf0b73e5bf2
brolita <byrnec3@rpi.edu>
2015-01-28 17:16:46 -0500

    Gogogo

--- Assets/GenerateRock.cs
+++ Assets/GenerateRock.cs
@@ -4,6 +4,9 @@ using System.Collections;
 public class GenerateRock : MonoBehaviour {
     public GameObject Rock;
     public float FallingFrequency = 2f;
+	public GameObject[] Set1;
+	public GameObject[] Set2;
+	public GameObject[] Set3;
 
 	// Use this for initialization
 	void Start () {
@@ -16,6 +19,24 @@ public class GenerateRock : MonoBehaviour {
 	}
 
     void GenerateARock() {
-        Instantiate(Rock);
-    }
+		float currentTime = GameObject.Find("GameState").GetComponent<GameState>().t;
+		if (currentTime < 20)
+		{
+			System.Random rnd = new System.Random();
+			int index = rnd.Next(0, Set1.Length);
+			Instantiate(Set1[index]);
+		}
+		else if(currentTime < 40)
+		{
+			System.Random rnd = new System.Random();
+			int index = rnd.Next(0, Set2.Length);
+			Instantiate(Set2[index]);
+		}
+		else
+		{
+			System.Random rnd = new System.Random();
+			int index = rnd.Next(0, Set3.Length);
+			Instantiate(Set3[index]);
+		}
+	}
 }
