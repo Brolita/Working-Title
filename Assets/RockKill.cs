8848331846c47a9836a112f8633e3f6b05161e68
brolita <byrnec3@rpi.edu>
2015-01-28 14:59:35 -0500

    player, rockkill test asset

---
+++ Assets/RockKill.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockKill : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Player")
+		{
+			other.GetComponent<Death>().HandelDeath();
+			Destroy(gameObject);
+		}
+	}
+}
