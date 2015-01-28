88bca1d3fe6eaf1c64d5217e7871534da8f6d443
Anli Ji <anli.azuresand@gmail.com>
2015-01-28 15:33:12 -0500

    Scene B is more like a scene

--- Assets/ARock.cs
+++ Assets/ARock.cs
@@ -5,7 +5,7 @@ public class ARock : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        transform.position = new Vector3(Random.Range(-3, 3), transform.position.y, transform.position.z);
+        transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 2.75f, transform.position.z);
 	}
 	
 	// Update is called once per frame
