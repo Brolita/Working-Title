8e3f3e24d9f5934a3d88fcab5973e955e782fcf8
Chris <cloutc@rpi.edu>
2015-01-28 16:18:37 -0500

    title screens and stuff (we still need an image for the title screen)

---
+++ Assets/Title.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Q) && Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.O) && Input.GetKey (KeyCode.P))
+		{				
+			Application.LoadLevel ("bScene");
+		}
+	}
+}
