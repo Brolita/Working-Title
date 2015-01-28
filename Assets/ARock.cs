9254c66aa6ab099734147e21b02ba9d6b6f0374a
Anli Ji <anli.azuresand@gmail.com>
2015-01-28 16:13:53 -0500

    Different things in different time intervel

--- Assets/ARock.cs
+++ Assets/ARock.cs
@@ -8,7 +8,23 @@ public class ARock : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+        float currentTime = GameObject.Find("GameState").GetComponent<GameState>().t;
         transform.position = new Vector3(Random.Range(-3.5f, 3.5f), 2.75f, transform.position.z);
+        if (currentTime > 20){
+            System.Random rnd = new System.Random();
+            int index = rnd.Next(0, 2);
+            gameObject.GetComponent<SpriteRenderer>().sprite = SpriteSetA[index];
+        }
+        else if (currentTime > 10){
+            System.Random rnd = new System.Random();
+            int index = rnd.Next(0, 4);
+            gameObject.GetComponent<SpriteRenderer>().sprite = SpriteSetB[index];
+        }
+        else {
+            System.Random rnd = new System.Random();
+            int index = rnd.Next(0, 6);
+            gameObject.GetComponent<SpriteRenderer>().sprite = SpriteSetC[index];
+        }
 	}
 	
 	// Update is called once per frame
